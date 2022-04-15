dc1919c867c8a192dd971b46e4f40f505e1bf9d6
Vasily Fateev <vasily.fat@gmail.com>
2022-04-15 15:27:57 +0300

    Add Scene Switch System and Loading Screen

---
+++ New Unity Project/Assets/Scripts/Global_Scripts/MissonInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MissonInfo
+{
+    private int missionID;
+    private bool isMissionComplete;
+    private int StarsCount;
+}
